61b67c6f89317cc1108d40756ca0572c1922efc4
Kevin Gallahan <kmgallahan@gmail.com>
2024-01-04 22:18:10 -0500

    Add project files.

---
+++ ReactiveWinUILibTest/AControl.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace ReactiveWinUILibTest;
+
+public sealed partial class AControl : UserControl
+{
+    public AControl()
+    {
+        this.InitializeComponent();
+    }
+}
